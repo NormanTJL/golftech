558df7225d99e766bccd4b432770189302ddd416
Norman <NormanTJL91@gmail.com>
2016-11-01 16:11:04 +0800

    added unedited image slider on main page

--- Proj1/ballmanage/index.php
+++ Proj1/ballmanage/index.php
@@ -72,12 +72,31 @@
 
         <div class="row">
             <div class="col-lg-12 text-left">
-                <br><br><br>
+                <div class="carousel slide" data-ride="carousel" align="center">
+                              <div class="carousel-inner">
+                                <div class="item active">
+                                    <a href="#"><img src="http://placehold.it/254x150/ff3546/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 1"></a>
+                                    <h4><small>Summer dress floral prints</small></h4>                                        
+                                </div>
+                                <div class="item">
+                                    <a href="#"><img src="http://placehold.it/254x150/3498db/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 2"></a>
+                                    <h4><small>Gold sandals with shiny touch</small></h4>                                        
+                                </div> 
+                                <div class="item">
+                                    <a href="#"><img src="http://placehold.it/254x150/2ecc71/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 3"></a>
+                                    <h4><small>Denin jacket stamped</small></h4>                                        
+                                </div>                              
+                              </div>
+                               
+                </div>
+                <br><br>
+                <table align="center"><tr><td colspan="2" width="65%"><img src="../media/lbms.jpg" align="left" alt="Large Ball Manangement System" style="width:150%;height:110%;"></td><td colspan="2"></td></tr></table>
                 <h2><font color="#336633">Ball Management Systems</font></h2>
                 <p class="h4">With our Ball Management System concept, every range operator is able to develop a cost efficient and easy-to-operate driving range. Through automation of the driving range and through sophisticated IT-Solutions a Ball Management System releases both time and resources to improve core business and customer relations.
 
 Comprehending the specific requirements for designing and operating a golf range, we are able to customize our Ball Management System to suit both the small country club and the large commercial range. We provide solutions that improve your range operations today and prepare you for the future. We have developed four technologies to help create an efficient Ball Management System for your range:</p>
-                <img src="../media/lbms.jpg" alt="Large Ball Manangement System" align="centre" style="width:60%;height:40%;">
+<br><br>
+                 <br><br><table align="center"><tr><td colspan="2" width="60%"><img src="../media/lbms.jpg" align="left" alt="Large Ball Manangement System" style="width:150%;height:110%;"></td><td colspan="2"></td></tr><tr><td colspan="2" width="60%"><img src="../media/lbms.jpg" align="left" alt="Large Ball Manangement System" style="width:150%;height:110%;"></td><td colspan="2"></td></tr></table>
             </div>
         </div>
         <!-- /.row -->
